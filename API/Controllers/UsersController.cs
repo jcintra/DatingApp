dfd86ed56069635a3d372eb54e4c6da54f257b30
João Cintra Silva <joaocintra@hotmail.com>
2020-11-04 19:12:58 +0000

    comentários

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet]   //recebe um pedido GET
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
